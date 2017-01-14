7a2a9f1a4b4eb9d17ff44e16e86d8e79b4ad44ac
sloshwoven <slosh.woven@gmail.com>
2017-01-14 09:29:10 -0500

    Add OpTable type and STANDARD_OP_TABLE
    Plus unit tests for its basic properties.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,37 @@
+pub type OpTable = [[u8; 10]; 10];
+
+pub const STANDARD_OP_TABLE: OpTable = [[0, 3, 1, 7, 5, 9, 8, 6, 4, 2],
+                                        [7, 0, 9, 2, 1, 5, 4, 8, 6, 3],
+                                        [4, 2, 0, 6, 8, 7, 1, 3, 5, 9],
+                                        [1, 7, 5, 0, 9, 8, 3, 4, 2, 6],
+                                        [6, 1, 2, 3, 0, 4, 5, 9, 7, 8],
+                                        [3, 6, 7, 4, 2, 0, 9, 5, 8, 1],
+                                        [5, 8, 6, 9, 7, 2, 0, 1, 3, 4],
+                                        [8, 9, 4, 5, 3, 6, 2, 0, 1, 7],
+                                        [9, 4, 3, 8, 6, 1, 7, 2, 0, 5],
+                                        [2, 5, 8, 1, 4, 3, 6, 7, 9, 0]];
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    const DIGITS: [u8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
     #[test]
-    fn it_works() {
+    fn sot_properties() {
+        assert_eq!(10, STANDARD_OP_TABLE.len(), "wrong number of rows");
+
+        for (row_i, row) in STANDARD_OP_TABLE.iter().enumerate() {
+            assert_eq!(10, row.len(), "wrong number of columns in row {}", row_i);
+            assert_eq!(0, row[row_i], "non-zero diagonal in row {}", row_i);
+            assert_digit_permutation(&row_i, &row);
+        }
+    }
+
+    fn assert_digit_permutation(row_i: &usize, row: &[u8; 10]) {
+        let mut row_sorted = row.clone();
+        row_sorted.sort();
+
+        assert_eq!(DIGITS, row_sorted, "invalid row {}", row_i);
     }
 }
